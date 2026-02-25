0b96804a5e5097aae9b1faace25d39dc62f53903
jeeva-tcm <jeeva.c@tickingminds.com>
2026-02-25 17:15:29 +0530

    added analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { inject } from '@angular/core';
+// import { Analytics } from '@vercel/analytics';
+import { track } from '@vercel/analytics';
+
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -26,4 +33,15 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 })
 export class AppComponent {
     title = 'ISO 20022 Validator';
+    constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        track('pageview', {
+          path: event.urlAfterRedirects
+        });
+      });
+  }
 }
+
+
