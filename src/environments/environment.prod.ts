2827ca57e3ef25d244e1c39d763b7afb8ca31ab2
santhan-tcm <santhan.v@tickingminds.com>
2026-03-06 10:39:15 +0530

    Update production environment settings
    Added API base URL and Firebase configuration for production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 // src/environments/environment.prod.ts
 // Production settings
 export const environment = {
-    production: true
+    production: true,
+     apiBaseUrl: 'https://iso20022generatorbackend.onrender.com',
+    firebaseConfig: {
+        apiKey: 'AIzaSyCt_KAzNydfejmWk6s-j34JYKlWcPUvvRo',
+        authDomain: 'iso20022-6b3d5.firebaseapp.com',
+        projectId: 'iso20022-6b3d5',
+        storageBucket: 'iso20022-6b3d5.firebasestorage.app',
+        messagingSenderId: '16264796734',
+        appId: '1:16264796734:web:6ccb9c8b3ff1cb70638f6f',
+        measurementId: 'G-FY23J2BKET'
+    }
 };
