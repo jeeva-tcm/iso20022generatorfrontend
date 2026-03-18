b1931a7650e34aa3c4413c5c2160af329ef44749
jeeva-tcm <jeeva.c@tickingminds.com>
2026-03-18 11:39:41 +0530

    changed DB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,13 @@ export const environment = {
     production: false,
     apiBaseUrl: 'http://127.0.0.1:8001',
     firebaseConfig: {
-        apiKey: 'AIzaSyCt_KAzNydfejmWk6s-j34JYKlWcPUvvRo',
-        authDomain: 'iso20022-6b3d5.firebaseapp.com',
-        projectId: 'iso20022-6b3d5',
-        storageBucket: 'iso20022-6b3d5.firebasestorage.app',
-        messagingSenderId: '16264796734',
-        appId: '1:16264796734:web:6ccb9c8b3ff1cb70638f6f',
-        measurementId: 'G-FY23J2BKET'
+        
+        apiKey: "AIzaSyAdEupd1RQVI8y2SNot4IB8BBubzGT5QU8",
+        authDomain: "iso-validator.firebaseapp.com",
+        projectId: "iso-validator",
+        storageBucket: "iso-validator.firebasestorage.app",
+        messagingSenderId: "246882613767",
+        appId: "1:246882613767:web:9f9f4343a851eb01f888a2",
+        measurementId: "G-WG5ZDJM0DT"
     }
 };
